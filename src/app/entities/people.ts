642d5b029a5dcb6a98634b0b8663f999df55db5b
Lluis <lluis.carrasco.martinez@stud.fh-campuswien.ac.at>
2019-09-15 15:37:39 +0200

    People class created

---
+++ src/app/entities/people.ts
@@ -0,0 +1,18 @@
+export class People {
+  name: string;
+  birthYear: string;
+  eyeColor: string;
+  gender: string;
+  harColor: string;
+  height: string;
+  mass: string;
+  skinColor: string;
+  homeWorld: string;
+  films: any;
+  species: any;
+  starships: any;
+  vehicles: any;
+  url: string;
+  created: string;
+  edited: string;
+}
